de6d68da99cac658e469169a80ecc5e9b0d6f9c7
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-08-29 08:14:30 +0900

    modify GetAddress

--- getaddrinfo/GetAddress.go
+++ getaddrinfo/GetAddress.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello Worldß")
+
+	addrs, err := net.InterfaceAddrs()
+	//name, err := os.Hostname()
+
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	cnt := 1
+
+	for _, get_addrs := range addrs {
+		if ipnet, ok := get_addrs.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To16() != nil {
+				fmt.Println(strconv.Itoa(cnt) + ":" + ipnet.IP.String())
+				cnt = cnt + 1
+			}
+		}
+	}
 }
